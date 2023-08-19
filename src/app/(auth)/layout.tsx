832f63e92a52f2b391dcee386016b7a27ec09fb2
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-08-19 18:58:23 +0530

    authentication with clerk

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from 'react';
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+    return <div className='flex items-center justify-center h-full'>{children}</div>;
+};
+
+export default Layout;
